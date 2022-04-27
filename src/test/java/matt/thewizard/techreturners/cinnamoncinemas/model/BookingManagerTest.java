d367e38cbf83032b5409d749c5ae89d5b50fc925
Matt DaSilva <matt_das@hotmail.co.uk>
2022-04-27 09:39:27 +0100

    Implement multiple allocations wrapping to any row

--- src/test/java/matt/thewizard/techreturners/cinnamoncinemas/model/BookingManagerTest.java
+++ src/test/java/matt/thewizard/techreturners/cinnamoncinemas/model/BookingManagerTest.java
@@ -60,25 +60,25 @@ public class BookingManagerTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/second-allocation-within-first-row.csv", numLinesToSkip = 1)
-    public void testSecondAllocationOfSeatsWithinFirstRow(int firstAllocation, int secondAllocation, String expectedSeatNumberList){
+    public void testSecondAllocationOfSeatsWithinFirstRow(int firstAllocation, int secondAllocation, String expectedSeatNumberList) {
         bookingManager.allocateSeats(firstAllocation);
         List<Seat> allocatedSeats = bookingManager.allocateSeats(secondAllocation);
 
-        assertEquals(secondAllocation,allocatedSeats.size());
+        assertEquals(secondAllocation, allocatedSeats.size());
 
         String[] expectedSeatNumbers = expectedSeatNumberList.split(" ");
-        for(int i = 0; i < allocatedSeats.size(); i++){
-            checkSeat(allocatedSeats.get(i),"A",expectedSeatNumbers[i]);
+        for (int i = 0; i < allocatedSeats.size(); i++) {
+            checkSeat(allocatedSeats.get(i), "A", expectedSeatNumbers[i]);
         }
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/first-row-wrap.csv", numLinesToSkip = 1)
-    public void testFirstRowWrap(String firstAllocationsList, int wrappingAllocation, String expectedSeatAllocationList){
+    public void testFirstRowWrap(String firstAllocationsList, int wrappingAllocation, String expectedSeatAllocationList) {
 
         //make first seat allocations
         String[] firstAllocations = firstAllocationsList.split(" ");
-        for(String allocation : firstAllocations)
+        for (String allocation : firstAllocations)
             bookingManager.allocateSeats(Integer.valueOf(allocation));
 
         //make the last allocation which should wrap to the second row
@@ -87,9 +87,51 @@ public class BookingManagerTest {
         assertEquals(wrappingAllocation, allocatedSeats.size());
 
         String[] expectedSeatAllocations = expectedSeatAllocationList.split(" ");
-        for(int i = 0; i < allocatedSeats.size(); i++)
-            checkSeat(allocatedSeats.get(i), expectedSeatAllocations[i].substring(0,1), expectedSeatAllocations[i].substring(1)); //expectedSeatAllocations have the format "A1"
+        for (int i = 0; i < allocatedSeats.size(); i++)
+            checkSeat(
+                    allocatedSeats.get(i),
+                    expectedSeatAllocations[i].substring(0, 1), //expectedSeatAllocations have the format "A1"
+                    expectedSeatAllocations[i].substring(1)
+            );
 
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/test-multiple-allocations.csv", numLinesToSkip = 1)
+    public void testMultipleAllocations(String firstAllocationList, int lastAllocation, String expectedSeatAllocationList) {
+        makeMultipleAllocations(firstAllocationList);
+        checkAllocation(lastAllocation, expectedSeatAllocationList);
+    }
+
+    /**
+     * Makes multiple allocations in the booking manager
+     *
+     * @param allocationsList - A List of the allocations to make as a space separated string e.g. "3 2 3 1"
+     */
+    private void makeMultipleAllocations(String allocationsList) {
+        String[] firstAllocations = allocationsList.split(" ");
+        for (String allocation : firstAllocations)
+            bookingManager.allocateSeats(Integer.valueOf(allocation));
+    }
+
+    /**
+     * Asserts that and allocation was successful
+     *
+     * @param allocation                 - the allocations to be made
+     * @param expectedSeatAllocationList - the expected seat allocations as a space separated string e.g "A1 A2 A3"
+     */
+    private void checkAllocation(int allocation, String expectedSeatAllocationList) {
+        List<Seat> allocatedSeats = bookingManager.allocateSeats(allocation);
+
+        assertEquals(allocation, allocatedSeats.size());
+
+        String[] expectedSeatAllocations = expectedSeatAllocationList.split(" ");
+        for (int i = 0; i < allocatedSeats.size(); i++)
+            checkSeat(
+                    allocatedSeats.get(i),
+                    expectedSeatAllocations[i].substring(0, 1), //expectedSeatAllocations have the format "A1"
+                    expectedSeatAllocations[i].substring(1)
+            );
     }
 
     private void checkSeat(Seat seat, Row row, SeatNumber seatNumber) {
